2b121729b19afc7e3f85849c1da98a93845db3a8
Zach Butcher <duke.fan04@yahoo.com>
2017-08-02 07:33:36 -0500

    added mapview and manager. go to location when load and click on compass to center location

--- Poke Go/ViewController.swift
+++ Poke Go/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate{
 
+    @IBOutlet weak var mapview: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            mapview.showsUserLocation = true
+            manager.startUpdatingLocation()
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 1000, 1000)
+            mapview.setRegion(region, animated: true)
+        }
+        else{
+            manager.requestWhenInUseAuthorization()
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+        // called evertime the location is updated
+        manager.stopUpdatingLocation()
+    }
+    
+    @IBAction func centerLocationTapped(_ sender: Any) {
+        if let coord = manager.location?.coordinate {
+            mapview.showsUserLocation = true
+            let region = MKCoordinateRegionMakeWithDistance(coord, 1000, 1000)
+            mapview.setRegion(region, animated: true)
+        }
+
     }
 }
 
